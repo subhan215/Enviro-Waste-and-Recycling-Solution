f69485b28351b4a842bdb7a2b827dfcf7bb22f55
Subhan Asif <asifsubhan904@gmail.com>
2025-01-06 14:56:09 +0500

    Update User_Schedules.js

--- src/app/components/user_dashboard/User_Schedules.js
+++ src/app/components/user_dashboard/User_Schedules.js
@@ -447,28 +447,16 @@ const SchedulesList = () => {
         {schedules.map((schedule) => (
           <div
             key={schedule.schedule_id}
-            className="p-6 sm:p-4 bg-white border border-custom-green rounded-lg shadow hover:shadow-md transition duration-200"
+            className="relative p-6 sm:p-4 bg-white border border-custom-green rounded-lg shadow hover:shadow-md transition duration-200"
           >
              {/* Chat Icon Button - Positioned at Top Right */}
-          <div className="relative top-2 right-2">
-            <button
-              onClick={() => handleInitiateChat(schedule.company_id, user_id)}
-              className="text-custom-green hover:text-green-700"
-            >
-             <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="h-5 w-5 text-black"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M4.848 2.771A49.144 49.144 0 0 1 12 2.25c2.43 0 4.817.178 7.152.52 1.978.292 3.348 2.024 3.348 3.97v6.02c0 1.946-1.37 3.678-3.348 3.97a48.901 48.901 0 0 1-3.476.383.39.39 0 0 0-.297.17l-2.755 4.133a.75.75 0 0 1-1.248 0l-2.755-4.133a.39.39 0 0 0-.297-.17 48.9 48.9 0 0 1-3.476-.384c-1.978-.29-3.348-2.024-3.348-3.97V6.741c0-1.946 1.37-3.68 3.348-3.97ZM6.75 8.25a.75.75 0 0 1 .75-.75h9a.75.75 0 0 1 0 1.5h-9a.75.75 0 0 1-.75-.75Zm.75 2.25a.75.75 0 0 0 0 1.5H12a.75.75 0 0 0 0-1.5H7.5Z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-            </button>
-          </div>
+             <button
+                onClick={() =>
+                  handleInitiateChat(schedule.company_id, user_id)
+                }
+                className="absolute top-4 right-4 p-2 rounded-full bg-gray-100 hover:bg-gray-200"
+              >
+                </button>
             {/* Schedule Information */}
             <p className="md:text-lg sm:text-md font-semibold text-custom-black">
               Date:{" "}
