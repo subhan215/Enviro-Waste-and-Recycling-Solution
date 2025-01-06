b2affd8301fbf591e844ce863e39aa0c4df0b7c5
Subhan Asif <asifsubhan904@gmail.com>
2025-01-06 17:52:13 +0500

    Update Alert.js

--- src/app/components/ui/Alert.js
+++ src/app/components/ui/Alert.js
@@ -24,7 +24,7 @@ const slideOut = keyframes`
 // Styled component for the alert container
 const AlertContainer = styled.div`
   position: fixed;
-  top: 50px;
+  top: 70px;
   right: 20px;
   z-index: 9999;
   display: flex;
